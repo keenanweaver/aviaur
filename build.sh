0a0348fb851806b3b24a7a5f9999ca705c22153f
Keenan Weaver <keenanweaver@protonmail.com>
2024-10-04 14:50:39 -0500

    build.sh: add displaylink

--- build.sh
+++ build.sh
@@ -18,6 +18,15 @@ rpm-ostree install screen
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
+# Install thinkfan, tlp, igt-gpu-tools and displaylink kernel module
+rpm-ostree install tlp thinkfan igt-gpu-tools /tmp/rpms/kmods/*evdi*.rpm
+
+#Disable negativo repo after installing displaylink, as it otherwise conflicts with RPM-fusion
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-multimedia.repo
+
+# Mask power-profiles-daemon in order for tlp to work correctly
+systemctl mask power-profiles-daemon.service
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
